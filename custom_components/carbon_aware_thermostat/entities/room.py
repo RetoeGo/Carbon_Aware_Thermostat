a39b0518221bdc764958320d69f2f96be096f5e0
Piotr Kranendonk <piotrdaniel@hotmail.nl>
2026-03-14 12:49:58 +0100

    Update simulation in room.py to use mpc

--- custom_components/carbon_aware_thermostat/entities/room.py
+++ custom_components/carbon_aware_thermostat/entities/room.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 import numpy as np
 import matplotlib.pyplot as plt
+from mpc import *
 
 
 @dataclass
@@ -15,10 +16,19 @@ class VirtualRoom:
         self.roomsize = size
         self.exposed_area = exposed_area
         self.thermostat = thermostat
+
+        self.window_conductivity = 1.5           # W / (m^2 * K)
+        self.insulation_conductivity = 0.04      # W / (m * K)
+        self.wall_thickness = 0.05               # m
+
+    def thermal_conductivity(self, window_percent=0.3):        
+        return ((1-window_percent)*self.insulation_conductivity/self.wall_thickness + window_percent*self.window_conductivity)
     
-    def update_temp(self, dt, T_out, show_data=False):
+    def update_temp(self, dt, T_out, window_percent=0.3, show_data=False):
+        assert 0 <= window_percent <= 1
+        
         Q_added = self.thermostat.power * dt
-        Q_loss_cond = 0.04 * self.exposed_area * (self.temp - T_out) / (0.05) * dt
+        Q_loss_cond = self.thermal_conductivity(window_percent) * self.exposed_area * (self.temp - T_out) * dt
         Q_loss_rad = 0
         Q_tot = Q_added - Q_loss_cond - Q_loss_rad
 
@@ -29,6 +39,16 @@ class VirtualRoom:
 
         if show_data:
             return Q_added, Q_loss_cond, Q_loss_rad, self.temp
+        
+    def generate_rls(self, dt):
+        b = dt / (1007 * self.roomsize * 1.225)
+        kl = self.thermal_conductivity()
+
+        c = b * kl * self.exposed_area
+        a = 1 - c
+        d = 0
+
+        return RLS(a,b,c,d)
 
 
 def main():
@@ -39,19 +59,25 @@ def main():
     w = 6           # room width
     l = 10          # room length
 
-    thermo = Thermostat(300)
-    myroom = VirtualRoom(T0, h*l*w, h*w, thermo)
+    dt = 60
+    time = np.arange(0, 3601, dt)
+
+    thermo = Thermostat(1000)
+    myroom = VirtualRoom(T0, h*l*w, h*l + h*w, thermo)
 
     room_temp = []
     Q_thermo = []
     Q_conduc = []
     Q_rad = []
 
-    dt = 60
-    time = np.arange(0, 3601, dt)
+    rls_model = myroom.generate_rls(dt)
 
     for t in time:
-        Qt, Qc, Qr, T = myroom.update_temp(dt, T_out, show_data=True)
+        input_power = mpc_control(rls_model, 20, myroom.temp, 21, T_out)[0]
+        thermo.power = input_power
+        print(f'input at time {t}: {input_power} W')
+        
+        Qt, Qc, Qr, T = myroom.update_temp(dt, T_out, window_percent=0.3, show_data=True)
         room_temp.append(T)
         Q_thermo.append(Qt)
         Q_conduc.append(Qc)
@@ -59,11 +85,16 @@ def main():
     
     fig, ax = plt.subplots(2,1)
 
-    ax[0].plot(time, room_temp, label='Room temperature (C)')
+    ax[0].plot(time/60, room_temp, label='Room temperature (C)')
     
-    ax[1].plot(time, Q_thermo, label='Heating (J)')
-    ax[1].plot(time, Q_conduc, label='Heat loss conduction (J)')
-    ax[1].plot(time, Q_rad, label='Heat loss radiation (J)')
+    ax[1].plot(time/60, np.array(Q_thermo)/dt, label='Heating (W)')
+    ax[1].plot(time/60, np.array(Q_conduc)/dt, label='Heat loss conduction (W)')
+    ax[1].plot(time/60, np.array(Q_rad)/dt, label='Heat loss radiation (W)')
+
+    ax[0].set_ylim(0, 30)
+
+    ax[0].set_xlabel('Time (min)')
+    ax[1].set_xlabel('Time (min)')
 
     ax[0].legend()
     ax[1].legend()
