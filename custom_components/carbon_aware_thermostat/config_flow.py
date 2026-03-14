646990e30fb1d56fdb20fb0da2dc46fe9a727622
Anhar Al Haydar <A.AlHaydar@student.tudelft.nl>
2026-03-14 15:55:24 +0100

    Extend config_flow

--- custom_components/carbon_aware_thermostat/config_flow.py
+++ custom_components/carbon_aware_thermostat/config_flow.py
@@ -1,16 +1,34 @@
+import voluptuous as vol
 from homeassistant import config_entries
+from homeassistant.helpers import selector
 
 from .const import DOMAIN
 
 
 class CarbonAwareThermostatConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
+        """Handle the initial step where the user adds the integration."""
+        errors = {}
+
         if user_input is not None:
+            # Here you could add a check to validate the API key
             return self.async_create_entry(
                 title="Carbon Aware Thermostat",
-                data={}
+                data=user_input
             )
 
-        return self.async_show_form(step_id="user")
+        # Define the schema for the UI form
+        data_schema = vol.Schema({
+            # API Key Input
+            vol.Required("api_key"): str,
+        })
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=data_schema,
+            errors=errors,
+        )
+
