a88d4efb3e0482aba15d471e66fb918131933ce0
Piotr Kranendonk <piotrdaniel@hotmail.nl>
2026-03-19 14:25:04 +0100

    Update MPC to only allow a few discrete power options

--- custom_components/carbon_aware_thermostat/entities/mpc.py
+++ custom_components/carbon_aware_thermostat/entities/mpc.py
@@ -7,7 +7,7 @@ class RLS:
         self.P = 100 * np.eye(4) # I matrix with 100 on each (i,i)
         self.lam = lam # forgetting factor
     
-    def update(self, T_k, u_k, T_outk, T_k1):
+    def update(self, T_k, u_k, T_outk, T_k1, show_data=False):
         phi = np.array([[T_k], [u_k], [T_outk], [1]])
         Tk1 = np.array([[T_k1]])
         #error
@@ -17,10 +17,12 @@ class RLS:
         #updates
         self.theta = self.theta + K @ e_k
         self.P = (self.P - K @ phi.T @self.P) / self.lam
+        if show_data:
+            return e_k[0]
 
-def mpc_control(RLS_model, N, T0, T_target, T_out, carbon_intensity):
-    weight_input = 0.5
-    weight_tracking = 5
+def mpc_control(RLS_model, N, T0, T_target, T_out, carbon_intensity, heat_stage):
+    weight_input = 1.5
+    weight_tracking = 100
     weight_smoothing = 0.0001
     
     cost = 0.0
@@ -28,7 +30,7 @@ def mpc_control(RLS_model, N, T0, T_target, T_out, carbon_intensity):
 
     # cp.Variable((dim_1, dim_2))
     T = cp.Variable((1, N + 1)) 
-    u = cp.Variable((1, N))
+    u = cp.Variable((1, N), integer=True)
 
     a, b, c, d = RLS_model.theta[:,0]
 
@@ -38,25 +40,27 @@ def mpc_control(RLS_model, N, T0, T_target, T_out, carbon_intensity):
     for k in range(N):
         T_outk = T_out[k]
         C_int = carbon_intensity[k]
+        T_targetk = T_target[k]
         T_k = T[:,k]
         T_k1 = T[:,k+1]
         u_k = u[:,k]
 
-        constraints += [T_k1 == a*T_k + b*u_k + c*T_outk + d]
+        constraints += [T_k1 == a*T_k + b*heat_stage*u_k + c*T_outk + d]
 
-        cost += weight_input * u_k * C_int
-        cost += weight_tracking * cp.square(T_k1 - T_target)
-        cost += weight_smoothing * cp.square(u_k)
+        cost += weight_input * u_k * heat_stage * C_int
+        cost += weight_tracking * cp.abs(T_k1 - T_targetk)
+        # cost += weight_smoothing * cp.square(u_k)
 
     # initial temperature    
     constraints += [T[:,0] == T0]
 
     # input constraints
     constraints += [u >= 0]
-    constraints += [u <= 1500]
+    constraints += [u <= 2]
+
 
     problem = cp.Problem(cp.Minimize(cost), constraints)
-    problem.solve(solver=cp.CLARABEL, **{"verbose": False})
+    problem.solve(solver=cp.GLPK_MI, **{"verbose": False})
 
     # return next input
-    return u[0, 0].value
+    return u[0, 0].value * heat_stage
--- custom_components/carbon_aware_thermostat/entities/room.py
+++ custom_components/carbon_aware_thermostat/entities/room.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 import numpy as np
 import matplotlib.pyplot as plt
 from mpc import *
+import time as tm
 
 
 @dataclass
@@ -52,65 +53,87 @@ class VirtualRoom:
 
 
 def main():
+    start_time = tm.time()
+    
     dt = 600
     time = np.arange(0, 60*60*24+1, dt)
-    N = 30
+    n = 3600//dt        # timesteps per hour
+    N = 18
     
     T0 = 15         # initial room temp (clesius)
     T_out = 5 + 10 * np.sin(np.pi / (60*60*24) * time)
     T_out = np.concatenate((T_out, T_out[:N]))
+
+    T_target = np.array(n*6*[15] + n*3*[21] + n*5*[18] + n*8*[21] + n*2*[15])
+    T_target = np.concatenate((T_target, T_target[:N+1]))
 
     # carbon_intensity = np.ones(len(time)) * 1
     # sun = len(time)//3
     # carbon_intensity[sun:sun*2] -= 7 * (np.sin(np.pi / (60*60*24) * time[sun:sun*2]) - np.sin(np.pi / (60*60*24) * time[sun]))
     # carbon_intensity = np.concatenate((carbon_intensity, carbon_intensity[:N]))
     
-    carbon_intensity = np.array(42*[1] + 12*[0.5] + 48*[0.1] + 12*[0.5] + (30+N+1)*[1])
+    
+    carbon_intensity = np.array(n*7*[1] + n*2*[0.5] + n*8*[0.1] + n*2*[0.5] + (n*5+N+1)*[1])
 
     h = 3           # room height
     w = 6           # room width
     l = 10          # room length
 
-    thermo = Thermostat(1000)
+    heat_stages = 500
+
+    thermo = Thermostat(0)
     myroom = VirtualRoom(T0, h*l*w, h*l + h*w, thermo)
 
     room_temp = []
     Q_thermo = []
     Q_conduc = []
     Q_rad = []
+    RLS_errors = []
 
     rls_model = myroom.generate_rls(dt)
 
     for i, t in enumerate(time):
-        input_power = mpc_control(rls_model, N, myroom.temp, 21, T_out[i:i+N], carbon_intensity[i:i+N])
+        T_k = myroom.temp
+        input_power = mpc_control(rls_model, N, T_k, T_target[i:i+N], T_out[i:i+N], carbon_intensity[i:i+N], heat_stages)
         thermo.power = input_power
         print(f'input at time {t}: {input_power} W')
         
-        Qt, Qc, Qr, T = myroom.update_temp(dt, T_out[i], window_percent=0.3, show_data=True)
-        room_temp.append(T)
+        Qt, Qc, Qr, T_k1 = myroom.update_temp(dt, T_out[i], window_percent=0.3, show_data=True)
+        room_temp.append(T_k1)
         Q_thermo.append(Qt)
         Q_conduc.append(Qc)
         Q_rad.append(Qr)
 
-        rls_model.update(myroom.temp, input_power, T_out[i], T)
+        # print(f'myroom.temp = {T_k}, T = {T_k1}')
+
+        e_k = rls_model.update(T_k, input_power, T_out[i], T_k1, show_data=True)
+        RLS_errors.append(e_k)
     
-    fig, ax = plt.subplots(2,1)
+    end_time = tm.time()
+    print(f'Total time: {round(end_time - start_time,3)} s')
+
+    fig, ax = plt.subplots(1,3, figsize=(12,4))
 
     ax[0].plot(time/3600, room_temp, label='Room temperature (C)')
-    ax[0].axhline(21, linestyle='dotted', color='red', label='target temp (C)')
+    ax[0].plot(time/3600, T_target[:-N], linestyle='dotted', color='red', label='target temp (C)')
     ax[0].plot(time/3600, T_out[:-N], label='Outside temperature (C)')
+    ax[0].plot(time/3600, 25*carbon_intensity[:-N], label='carbon intensity (*25)')
     
     ax[1].plot(time/3600, np.array(Q_thermo)/dt, label='Heating (W)')
     ax[1].plot(time/3600, np.array(Q_conduc)/dt, label='Heat loss conduction (W)')
     ax[1].plot(time/3600, np.array(Q_rad)/dt, label='Heat loss radiation (W)')
     ax[1].plot(time/3600, 1000*carbon_intensity[:-N], label='carbon intensity (*1000)')
 
+    ax[2].plot(time/3600, RLS_errors)
+
     ax[0].set_ylim(0, 30)
 
+    ax[0].set_xlabel('Time (hours)')
     ax[1].set_xlabel('Time (hours)')
+    ax[2].set_xlabel('Time (hours)')
 
     ax[0].legend()
-    ax[1].legend()
+    # ax[1].legend()
 
     plt.show()
 
